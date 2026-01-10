96c96f23d46de09549fbfb9728373d46f3a6b571
Probroferxdxd . <probrofer@gmail.com>
2026-01-10 15:22:37 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'https://github.com/Probroferxdxd/herramientas-estudio',
   plugins: [react(), tailwindcss()],
 })
